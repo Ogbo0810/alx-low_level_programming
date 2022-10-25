90442a7b32400392b5b368876c6e1b1f156b8538
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-25 11:06:15 -0700

    Added file: 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - starting point for program execution
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
