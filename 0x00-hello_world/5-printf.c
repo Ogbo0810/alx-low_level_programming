e59af30697f439ec0bc701e6727db0015c17bbd5
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-25 11:22:43 -0700

    Added file: 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - starting point for programme execution
+ * printf - prnting string variables to stdout
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
