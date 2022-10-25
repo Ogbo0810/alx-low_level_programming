c221434052ea4e44846c11d76f4c462dec1023cf
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-25 12:37:28 -0700

    Added file: 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - starting point of programme execution
+ *
+ * Return: Always 1(Error)
+ **/
+
+int main(void)
+{
+
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);	
+	return(1);
+}
