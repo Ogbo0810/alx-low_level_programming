03900b7068be1d3f7d6bda3994e7ce86d7bae455
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-26 02:59:27 -0700

    changed content of file: 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  * main - starting point of programme execution
  *
  * Return: zero
- * */
+ */
 
 int main(void)
 {
@@ -19,12 +19,12 @@ int main(void)
 		printf("%d is positive", n);
 	}
 	else if (n == 0)
-        {
-                printf("%d is zero", n);
-        }
+	{
+		printf("%d is zero", n);
+	}
 	else
-        {
-                printf("%d is negative", n);
-        }
+	{
+		printf("%d is negative", n);
+	}
 	return (0);
 }
