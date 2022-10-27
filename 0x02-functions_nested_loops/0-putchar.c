7e582627c2cc71785b28bd72b13c006f58406aa1
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-27 15:22:59 -0700

    Added file: main.h, deleted file: 0-putchar, edited content of file: 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - starting point of programme execution
@@ -8,6 +9,7 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_putchar');
+	putchar('\n');
 	return (0);
 }
