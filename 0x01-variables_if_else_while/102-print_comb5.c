bf88374e9613e9c1b385070b9e4c6f3c00c747f1
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-27 10:58:59 -0700

    Added file: 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of programme execution
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int f, s;
+
+	for (f = 0; f <= 98; f++)
+	{
+		for (s = f + 1; s <= 99; s++)
+		{
+			putchar((f / 10) + '0');
+			putchar((f % 10) + '0');
+			putchar(' ');
+			putchar((s / 10) + '0');
+			putchar((s % 10) + '0');
+
+			if (f == 98 && s == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
