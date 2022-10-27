4eb0d7ec769d3c58b4fb864ec62cffc7edc8d93f
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-27 07:30:59 -0700

    Added file: 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - starting point of programme execution
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	char alpha;
+
+i	alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+
+	putchar('\n');
+	return (0);
+}
