4b8f8f61f0b47cfde1b64b6f32564bcc81fa15a4
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-27 19:46:56 -0700

    Added file: 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for alpabetic characters
+ * @c: parameter being checked
+ *
+ * Return: 1 if c is letter, 0 otherwise
+ */
+int _isalpha(int c);
+{
+	char i;
+	char j;
+	int result;
+
+	result = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (j = 'A'; j <= 'Z'; j++)
+		{
+			if (i == c || j == c)
+			{
+				result = 1;
+			}
+		}
+	}
+	return (result);
+}
