40b4cbc44c5a256fa419d790ed21569a4078973b
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-27 18:44:03 -0700

    added file: 1-alphabet.c, changed content of file: main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,34 +2,18 @@
 #include "main.h"
 
 /**
- * main - starting point of programme execution
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
  *
- * Return: zero
+ * Return: void
  */
-int main(void)
+void print_alphabet(void);
 {
-	char word[8] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
+	char i;
+	
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(word[i]);
+		_putchar(i);
 	}
 	_putchar('\n');
-
-	return (0);
 }
-
-/**
- * _putchar - It prints a word.
- * @c: only operand
- *
- * Description: This prints the only arcgument inside it
- * Return: the parameter
- */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
-
 
