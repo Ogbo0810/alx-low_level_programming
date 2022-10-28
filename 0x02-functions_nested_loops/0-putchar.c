f58d2bc06289ab325d7776d597be7fa65d6ea8a1
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-27 18:22:05 -0700

    changed content of file: 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 
 /**
  * main - starting point of programme execution
- * Description: It prints the word _putchar, followed by a new line.
+ *
  * Return: zero
  */
 int main(void)
@@ -11,7 +11,7 @@ int main(void)
 	char word[8] = "_putchar";
 	int i;
 
-	for (i = 0; i<8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(word[i]);
 	}
@@ -20,7 +20,16 @@ int main(void)
 	return (0);
 }
 
+/**
+ * _putchar - It prints a word.
+ * @c: only operand
+ *
+ * Description: This prints the only arcgument inside it
+ * Return: the parameter
+ */
 int _putchar(char c)
 {
 		return (write(1, &c, 1));
 }
+
+
