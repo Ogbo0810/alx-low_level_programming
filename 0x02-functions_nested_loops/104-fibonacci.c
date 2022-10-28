47856ced8a8cfd1120919ce2cae7a2a98b61bcea
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-28 08:29:05 -0700

    Added file: 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 
 /**
  * main - starting point of file execution
@@ -7,23 +8,39 @@
  */
 int main(void)
 {
-	unsigned long int c, n, sum, i;
+	unsigned long int c, n, sum, i, h1, h2, h3, f1, f2;
 
-	c = 1;
-	n = 2;
+	c = 1; /*current*/
+	n = 2;/*next*/
+	f1 = 0;
+	f2 = 0;
 
 	printf("%lu, %lu, ", c, n);
 
 	for (i = 3; i <= 98; i++)
-	{
-		sum = (c + n);
-		printf("%lu", sum);
+	{	sum = c + n;
+		if (sum > LARGEST || f1 > 0 || f2 > 0)
+		{
+			h1 = sum / LARGEST;
+			h2 = sum % LARGEST;
+			h3 = sum + h1;
+			f1 = f2;
+			f2 = h3;
+			c = n;
+			n = h2;
+			printf("%lu%010lu", f2, n);
+		}
+		else
+		{
+			h2 = sum;
+			c = n
+			n = h2;
+			printf("%lu", n);
+		}
 		if (i < 50)
 		{
 			printf(", ");
 		}
-		c = n;
-		n = sum;
 	}
 	printf("\n");
 	return (0);
