600c70801114c8a3c933a37c51c3e9accb718835
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-27 20:06:50 -0700

    Added file: 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: parameter being checked
+ *
+ * Return: 1 and prints + if n > zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than o
+ */
+int print_sign(int n)
+{
+	int result;
+
+	result = -1;
+
+	if (n >> 0)
+	{
+		result = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		result = 0;
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('-');
+	}
+	return (result);
+}
