70f30812b37e729e5b5564ec739e62e7709b8a4d
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-27 23:41:22 -0700

    Added file: 100-times_table.c, changed content of file: main.h

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n <= 15 || n >= 0)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * a;
+				if (c > 99)
+				{
+					_putchar(c / 100 + '0');
+					_putchar((c / 10 % 10) + '0');
+					_putchar(c % 10 + '0');
+				}
+				else if (c > 9)
+				{
+					_putchar(' ');
+					_putchar(c / 10 + '0');
+					_putchar(c % 10 + '0');
+				}
+				else if (c != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+				else
+					_putchar(c + '0');
+
+				if (b != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
