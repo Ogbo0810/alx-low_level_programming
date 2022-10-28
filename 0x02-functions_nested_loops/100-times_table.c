9fd4d2044b6b94caf1a5b6187ad713c6fa5409df
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-27 23:54:48 -0700

    Added file: 100-times_table.c, changed content of file: main.h

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ void print_times_table(int n)
 				_putchar(c / 10 + '0');
 				_putchar(c % 10 + '0');
 			}
-			else if (c != 0)
+			else if (c >= 0)
 			{
 				_putchar(' ');
 				_putchar(' ');
