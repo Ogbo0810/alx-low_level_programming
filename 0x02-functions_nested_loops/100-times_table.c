c30b34c21a84cfac482f4bc6e0ae90599558dc69
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-27 23:48:33 -0700

    Added file: 100-times_table.c, changed content of file: main.h

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,41 +9,42 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	if (n <= 15 || n >= 0)
+	if (n > 15 || n < 0)
 	{
-		for (a = 0; a <= n; a++)
+		return;
+	}
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
 		{
-			for (b = 0; b <= n; b++)
+			c = a * b;
+			if (c > 99)
 			{
-				c = a * a;
-				if (c > 99)
-				{
-					_putchar(c / 100 + '0');
-					_putchar((c / 10 % 10) + '0');
-					_putchar(c % 10 + '0');
-				}
-				else if (c > 9)
-				{
-					_putchar(' ');
-					_putchar(c / 10 + '0');
-					_putchar(c % 10 + '0');
-				}
-				else if (c != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(c + '0');
-				}
-				else
-					_putchar(c + '0');
-
-				if (b != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(c / 100 + '0');
+				_putchar((c / 10 % 10) + '0');
+				_putchar(c % 10 + '0');
 			}
-			_putchar('\n');
+			else if (c > 9)
+			{
+				_putchar(' ');
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+			else if (c != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else
+				_putchar(c + '0');
+
+			if (b != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
 }
