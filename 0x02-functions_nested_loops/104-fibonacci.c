7d82ea455aed125e3bbf3902c59a94795a60bf37
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-28 07:42:23 -0700

    Added file: 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of file execution
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	unsigned long int c, n, sum, i;
+
+	c = 1;
+	n = 2;
+
+	printf("%lu, %lu, ", c, n);
+
+	for (i = 3; i <= 98; i++)
+	{
+		sum = (c + n);
+		printf("%lu", sum);
+		if (i < 50)
+		{
+			printf(", ");
+		}
+		c = n;
+		n = sum;
+	}
+	printf("\n");
+	return (0);
+}
