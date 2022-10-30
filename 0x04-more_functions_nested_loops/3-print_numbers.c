842ba940437669272e8a3c722333a7b3a282f6ea
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-30 13:53:54 -0700

    Added file: 3-print_numbers.c | edited file: main.h

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,13 +6,13 @@
  * Return: 0
  */
 
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-			_putchar(i);
+		_putchar(i);
 	}
 	_putchar('\n');
 	return (0);
