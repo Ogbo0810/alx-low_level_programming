1c8e46694446d5386352989a360bc03f81d18bbc
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-30 15:26:52 -0700

    Added file: 6-print_line.c | edited file: main.h

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,6 +2,7 @@
 
 /**
 * print_line - prints 10 times the numbers, from 0 to 14
+* @n: number of underscores to print per line
 *
 * Return: void
 */
@@ -9,11 +10,13 @@
 void print_line(int n)
 {
 	int i;
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+
+/*	if (n <= 0)
+*	{
+*		_putchar('\n');
+*	}
+*/
+	if (!(n <= 0))
 	{
 		for (i = 1; i <= n; i++)
 		{
