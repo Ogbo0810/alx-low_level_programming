ee0968d4fbfb89190d36e2d017e2e20680b693e9
Ogbo0810 <ogbodadeboe@gmail.com>
2022-10-30 17:45:39 -0700

    Added file: 9-fizz_buzz.c, 9-main.c | edited file: main.h

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* main - starting point of programme execution
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+				printf("%d\n", i);
+			else
+				printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
