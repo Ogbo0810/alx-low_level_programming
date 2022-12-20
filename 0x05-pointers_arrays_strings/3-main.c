9a9cfa43ed90c1ef4b1fcb17eae90366d86b8f10
Alberta Ogbo Dadeboe <ogbodadeboe@gmail.com>
2022-12-20 03:14:31 -0800

    Added file: 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - verify code for 3-main.c
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I fear the lack of computers";
+	_puts(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _puts - prints a string followed by a new line
+* @str: string to be printed
+*
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
